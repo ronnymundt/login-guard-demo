f718c9057441df7080f3618fde627a6c2053bcc0
Ronny Mundt <r.mundt@gmx.net>
2023-01-28 13:24:05 +0100

    Guard Boilerplate Service entfernt

--- src/app/classes/router-paths.ts
+++ src/app/classes/router-paths.ts
@@ -1,13 +1,20 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from '../components/login/login.component';
 import { HomeComponent } from '../components/home/home.component';
-import { LoginGuard } from '../guards/login.guard';
+import { inject } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { ILoginState } from '../interfaces/login.interface';
+import { SelectLogin } from '../selectors/login.selectors';
+import { map } from 'rxjs';
 
-export class RouterPaths {
+export class RouterPaths {         
     public routes: Routes = [
         { path: 'login', component: LoginComponent },
-        { path: 'home', component: HomeComponent, canActivate: [LoginGuard] },
-
+        { 
+            path: 'home', 
+            component: HomeComponent, 
+            canActivate: [() => inject(Store<ILoginState>).select(SelectLogin).pipe(map((x: ILoginState) => x.isSuccess))] 
+        },
         //
         { path: '**', redirectTo: 'login' }
     ]
